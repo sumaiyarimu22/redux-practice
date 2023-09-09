c8a3bc16e619bd763f743d11a0a4d6bbf09ade8a
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-09-09 07:56:26 +0600

    add multi middleware and and redux-logger

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,17 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
+import myLogger from "./middlewars/mylogger";
+import logger from "redux-logger";
 
-//create out first middleware
-const myLogger = (store) => (next) => (action) => {
-  console.log(`action:${JSON.stringify(action)}`);
-  console.log(`before:${JSON.stringify(store.getState())}`);
-
-  const upcommingState = [action].reduce(rootReducer, store.getState());
-  console.log(`upcomming state:${JSON.stringify(upcommingState)}`);
-
-  return next(action);
-};
-
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
 
 export default store;
