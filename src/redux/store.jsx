28ff346411f75984771a464847928a611fd1813c
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-09-10 08:04:10 +0600

    update

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 import myLogger from "./middlewars/mylogger";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, myLogger))
+);
 
 export default store;
