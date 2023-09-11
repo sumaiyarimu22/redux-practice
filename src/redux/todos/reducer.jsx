7a49cbcea12917e5c5e1378de288b545b0b629cf
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-09-11 09:35:49 +0600

    filters reducer added

--- src/redux/todos/reducer.jsx
+++ src/redux/todos/reducer.jsx
@@ -67,7 +67,7 @@ const reducer = (state = initialState, action) => {
       return state.filter((todo) => !todo.completed);
 
     default:
-      break;
+      return state;
   }
 };
 export default reducer;
